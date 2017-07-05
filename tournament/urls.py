1194d89b82ccacbe1b418fbfbd31c19a469e945e
Andrey Ezhov <rybakisa@gmail.com>
2017-07-06 02:32:50 +1000

    move login/logout urls from global

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from tournament.views import MainPage
+from tournament.views import MainPage, LoginView, LogoutView
 
 urlpatterns = [
     url(r'^$', MainPage.as_view(), name='mainpage'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
