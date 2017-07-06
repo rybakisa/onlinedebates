164a352e90575b1b13026958cf2d7a80c8741c4d
Администратор <gorbunovataisia@gmail.com>
2017-07-07 01:02:51 +1000

    style schedule

--- tournament/static/common.js
+++ tournament/static/common.js
@@ -5,4 +5,19 @@ $(document).ready(function() {
   $(".page-header__smoke-rooms-title").click(function() {
     $(".page-header__smoke-room-container").slideToggle();
   });
+  $("#moscow-title").hover(function() {
+    $("#moscow").toggle();
+  });
+  $("#ekb-title").hover(function() {
+    $("#ekb").toggle();
+  });
+  $("#omsk-title").hover(function() {
+    $("#omsk").toggle();
+  });
+  $("#nsk-title").hover(function() {
+    $("#nsk").toggle();
+  });
+  $("#vdk-title").hover(function() {
+    $("#vdk").toggle();
+  });
 });
