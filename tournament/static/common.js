979362867c8b02a7fd0e4b2806c8af375bc0293a
Администратор <gorbunovataisia@gmail.com>
2017-07-06 22:53:21 +1000

    add animation, minor style changes

---
+++ tournament/static/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".page-header__schedule").click(function() {
+    $(".page-header__schedule-container").slideToggle();
+  });
+  $(".page-header__smoke-rooms-title").click(function() {
+    $(".page-header__smoke-room-container").slideToggle();
+  });
+});
